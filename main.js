b6b951fa8bafec0a83649c7c38a1356c964a9d93
T0BY-Parakeet <takahiromazisugoi@gmail.com>
2024-09-19 05:21:17 +0000

    9月19日 javascriptの導入

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector("h1").textContent ="自分の名前"
+
+document.querySelector("h2").textContent ="飛松隆寛"
+
+//documentとは！！
+/* htmlファイルで書かれているコードを一つのオブジェクトとしてみなす */
+
+//DOM(ドム)
+/* ドキュメントオブジェトモデル　(Document Object Model)の略で、webページの要素やコンテンツなどをツリー構造で表現したデータモデル */
+
+//上記コードの意味
+//document.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent = "飛松隆寛"
