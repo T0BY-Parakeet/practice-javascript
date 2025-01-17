80b4993575b763595d4bed24e855f283dd21a67a
T0BY-Parakeet <takahiromazisugoi@gmail.com>
2025-01-17 06:17:27 +0000

    数当てゲームの追加

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function  clickCounterGame(){
 
     const gameContainer = document.getElementById("game-container");
 
-
     let button1 = document.createElement("button");
     let button2 = document.createElement("button");
     let reset = document.createElement("button");
@@ -13,25 +12,30 @@ function  clickCounterGame(){
     button2.textContent = "+10ボタン";
     reset.textContent  = "リセット";
 
-
     let counter = document.createElement("p");
-
     counter.textContent = count;
 
-
     button1.addEventListener("click",function(){
+        if(count <= 90){
         count++;
+        }
         counter.textContent = count;
 
     })
 
     button2.addEventListener("click",function(){
-        count = count + 10;
-        counter.textContent = count;
+        
+        if(count <= 90){
+            count = count + 10;
+        }
 
+        counter.textContent = count;
     })
 
-    counter.addEventListener("click",function(){
+
+
+    reset.addEventListener("click",function(){
+       
         count = 0;
         counter.textContent = count;
 
@@ -45,3 +49,26 @@ function  clickCounterGame(){
 }
 
 clickCounterGame();
+
+
+
+function starNumberGuessGame(){
+
+    const gameContainer = document.getElementById("game-container");
+
+    const randomNumber = Math.floor (Math.random() * 100) + 1;
+
+    let message = document.createElement('p');
+
+    let input = document.createElement('input');
+    message.textContent = randomNumber;
+
+    input.type = 'number';
+
+    input.placeholder = '好きな数字を入力してください (1-100)'
+
+    gameContainer.appendChild(input);
+    gameContainer.appendChild(message);
+}
+
+starNumberGuessGame();
